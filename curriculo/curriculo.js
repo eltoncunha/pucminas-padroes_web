ded4ae5c58b439d3348fda455ec0816cb5b13fd2
Elton Vieira Cunha <elton13_1@hotmail.com>
2014-10-29 11:00:33 -0200

    Curriculo Elton
    Currículo de Elton em HTML, CSS e JS

---
+++ curriculo/curriculo.js
@@ -0,0 +1,11 @@
+﻿// JavaScript Document
+
+function showHide(ID) {
+	if (document.getElementById(ID).style.display == "none") {
+		document.getElementById(ID).style.display= "";
+		document.getElementById(ID + "_span").innerHTML= "[-]";
+	}else{
+		document.getElementById(ID).style.display = "none";
+		document.getElementById(ID + "_span").innerHTML= "[+]";
+	}
+}
